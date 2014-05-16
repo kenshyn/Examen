e150f9385a79b0dfc848e7ba043888302051d498
kenshyn <victormonell@hotmail.com>
2014-05-16 17:37:11 +0200

    Sumar descarga

--- controller/Main.py
+++ controller/Main.py
@@ -42,12 +42,10 @@ if(option == "4"):
     print ("gracias por la descarga")
 if(option == "5"):
     nombre = input("Que aplicacion quieres comentar?")
-    DataBase.modificaApp(nombre, 7)
-    print ("gracias por el comentario")
+
 if(option =="6"):
     print("aun no")
 if(option == "7"):
     print("nope")
 if(option == "8"):
     nombreProv = input("Introduce el nombre del proveedor a mostrar")
-    DataBase.llegeixAppsperProveedor(nombreProv)
